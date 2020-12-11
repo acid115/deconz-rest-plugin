4ce683bc7709cce54de255599fffb329da063748
acid115 <other@menmor.de>
2020-12-11 22:33:09 +0100

    dsad

--- rest_lights.cpp
+++ rest_lights.cpp
@@ -734,7 +734,7 @@ int DeRestPluginPrivate::setLightState(const ApiRequest &req, ApiResponse &rsp)
                     valueOk = true;
                     hasCt = true;
                     targetCt = (ctMin < 500 && ct < ctMin) ? ctMin : (ctMax > ctMin && ct > ctMax) ? ctMax : ct;
-                    DBG_Printf(DBG_INFO, "Tommy: hasct! targetCt: %d\n", targetCt);
+                    //DBG_Printf(DBG_INFO, "Tommy: hasct! targetCt: %d\n", targetCt);
                 }
             }
         }
@@ -756,7 +756,7 @@ int DeRestPluginPrivate::setLightState(const ApiRequest &req, ApiResponse &rsp)
                     ct += ctInc;
                     ct = ct < 0 ? 0 : ct > 0xFEFF ? 0xFEFF : ct;
                     targetCt = (ctMin < 500 && ct < ctMin) ? ctMin : (ctMax > ctMin && ct > ctMax) ? ctMax : ct;
-                    DBG_Printf(DBG_INFO, "Tommy: hasctInc! targetCtInc: %d\n", targetCt);
+                    //DBG_Printf(DBG_INFO, "Tommy: hasctInc! targetCtInc: %d\n", targetCt);
                 }
             }
         }
@@ -770,7 +770,7 @@ int DeRestPluginPrivate::setLightState(const ApiRequest &req, ApiResponse &rsp)
                 valueOk = true;
                 hasHue = true;
                 targetHue = hue; // Funny: max CurrentHue is 0xFE, max EnhancedCurrentHue is 0xFFFF
-                DBG_Printf(DBG_INFO, "Tommy: hashue! targethue: %d\n", targetHue);
+                //DBG_Printf(DBG_INFO, "Tommy: hashue! targethue: %d\n", targetHue);
             }
         }
         else if (param == "sat" && taskRef.lightNode->item(RStateHue) && taskRef.lightNode->item(RStateSat))
@@ -1144,9 +1144,9 @@ int DeRestPluginPrivate::setLightState(const ApiRequest &req, ApiResponse &rsp)
         copyTaskReq(taskRef, task);
         quint16 targetCtForChange = targetCt;
 
-        DBG_Printf(DBG_INFO, "Tommy: hasCT\n");
-        DBG_Printf(DBG_INFO, "Tommy: Vendor: %d\n", task.lightNode->manufacturerCode());
-        DBG_Printf(DBG_INFO, "Tommy: reqmode: %s\n", req.mode);
+        //DBG_Printf(DBG_INFO, "Tommy: hasCT\n");
+        //DBG_Printf(DBG_INFO, "Tommy: Vendor: %d\n", task.lightNode->manufacturerCode());
+        //DBG_Printf(DBG_INFO, "Tommy: reqmode: %s\n", req.mode);
 
         //correct ikea ct 
         if (task.lightNode->manufacturerCode() == VENDOR_IKEA && 
